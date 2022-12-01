a7c83118331a7310206bfa3ac355aaf3d928fb0e
Lucas Dindault <lucas@creabilis.com>
2022-12-01 11:30:01 +0100

    setup cognito register

---
+++ ybook/src/components/Register.tsx
@@ -0,0 +1,65 @@
+import React, { SyntheticEvent, useRef, useState } from 'react'
+import userPool, {userData} from '../UserPool';
+import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
+
+export interface RegisterPageProps {}
+
+const Register: React.FC<RegisterPageProps> = () => {
+  const [name, setName] = useState('');
+  const [givenName, setGivenName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  // cognitoUser.confirmRegistration('123456789', true, function(err, result) {
+  //   if (err) {
+  //     alert(err.message || JSON.stringify(err));
+  //     return;
+  //   }
+  //   console.log('call result :' + result);
+  // });
+
+  const handleSubmit = (e : SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email", Value:email});
+    var attributeName = new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name", Value: name});
+    var attributeGivenName = new AmazonCognitoIdentity.CognitoUserAttribute({Name:'given_name', Value:givenName});
+    var attributeList: AmazonCognitoIdentity.CognitoUserAttribute[] = [];
+    attributeList.push(attributeEmail);
+    attributeList.push(attributeName);
+    attributeList.push(attributeGivenName);
+
+    userPool.signUp(email, password, attributeList, null as any, function(
+      err,
+      result
+    ) {
+      if (err) {
+        alert(err.message || JSON.stringify(err));
+        return;
+      }
+      var cognitoUser = result?.user;
+      console.log('user name is ' + cognitoUser?.getUsername());
+    });
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Last Name</label>
+          <input type="text" name="name" id="name" onChange={(e) => setName(e.currentTarget.value)} value={name} required/>
+          <label htmlFor="given_name">First Name</label>
+          <input type="text" name="given_name" id="given_name" onChange={(e) => setGivenName(e.currentTarget.value)} value={givenName} required/>
+          <label htmlFor="email">Email</label>
+          <input name='email' type="email" onChange={(e) => setEmail(e.currentTarget.value)} value={email} required/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" onChange={(e) => setPassword(e.currentTarget.value)} value={password} required/>
+          <button type='submit'>submit</button>
+        </form>
+      </div>
+      <br />
+    </>
+  )
+}
+
+export default Register
