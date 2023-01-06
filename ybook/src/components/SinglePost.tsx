4fad50679f0bdd0b5db1e0d6ae35b025401f4f05
Lucas Dindault <lucas@creabilis.com>
2023-01-06 15:36:24 +0100

    add like post fonction

--- ybook/src/components/SinglePost.tsx
+++ ybook/src/components/SinglePost.tsx
@@ -1,40 +1,63 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Post } from './ListPosts'
 import { FiThumbsUp } from 'react-icons/fi'
 import { FaRegComment } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { RiShareForward2Line } from 'react-icons/ri'
+import _ from 'lodash'
 
 interface Props {
   posts: Post[];
+  setPosts : any;
 }
 
-const triggerLike = () => {}
-const triggerComments = () => {}
-const triggerShare = () => {}
+const SinglePost: React.FC<Props> = ({ posts, setPosts }) => {
+  const addLike = async (postId: number) => {
+    const response = await fetch(`http://localhost:3100/postLikes`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: 25,
+        postId
+      })
+    })
 
-const SinglePost : React.FC<Props> = ({posts}) => {
+    fetch(`http://localhost:3100/post/${postId}`)
+      .then(response => response.json())
+      .then(
+        res => {
+          let newPosts = posts
+          newPosts[_.findIndex(posts, { id: postId })] = res
+          setPosts([...newPosts])
+        })
+  }
+
+  const triggerComments = () => { }
+  const triggerShare = () => { }
+
   return (
     <div>
       {posts?.map((post) => {
         return (
-          <div className='post'>
+          <div className='post' key={post.id} id={`${post.id}`}>
             <div className="post-header">
-              <img src="https://i.pravatar.cc/48" alt="" className='post-header__image' width={48} height={48} loading="lazy"/>
+              <img src="https://i.pravatar.cc/48" alt="" className='post-header__image' width={48} height={48} loading="lazy" />
               <div className='post-header__content'>
                 <span className='post-header__title'>{post.user['firstname']} {post.user['lastname']}</span>
-                <span className='post-header__date'>Posté le {new Intl.DateTimeFormat('fr', {dateStyle: 'medium'}).format(new Date(post.createdAt))}</span>
+                <span className='post-header__date'>Posté le {new Intl.DateTimeFormat('fr', { dateStyle: 'medium' }).format(new Date(post.createdAt))}</span>
               </div>
             </div>
             <div className='post-content'>
               <p className='post-content__text'>{post.htmlContent}</p>
-              <img src="https://res.cloudinary.com/drxtvqede/image/upload/v1672993634/dog_d70r0m.jpg" alt="" className='post-content__image img-fluid' loading='lazy'/>
+              <img src="https://res.cloudinary.com/drxtvqede/image/upload/v1672993634/dog_d70r0m.jpg" alt="" className='post-content__image img-fluid' loading='lazy' />
             </div>
             <div className="post-footer">
               <div className="post-footer__likes">
-                <Link to='#' onClick={triggerLike}>
+                <Link to='#' onClick={() => addLike(post.id)}>
                   <FiThumbsUp className='post-footer__likes-icon' />
-                  <span className='post-footer__likes-count'>26</span>
+                  <span className='post-footer__likes-count'>{Object.keys(post.postLikes).length}</span>
                 </Link>
               </div>
               <div className="post-footer__comments">
