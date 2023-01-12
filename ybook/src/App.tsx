6bd5aa16fbad7cb9512807f7467c2b9a1ef1a4fb
Lucas Dindault <lucas@creabilis.com>
2023-01-12 10:35:45 +0100

    end post integration

--- ybook/src/App.tsx
+++ ybook/src/App.tsx
@@ -6,26 +6,21 @@ import {Routes, Route} from 'react-router-dom';
 import Header from './Layouts/Header';
 import Footer from './Layouts/Footer';
 import ConfirmRegister from './components/ConfirmRegister';
-
-const location = window.location.pathname;
+import Home from './Layouts/Home';
 
 function App() {
-  if (location === '/register' || location === '/login') {
-    return (
-      <Routes>
-        <Route path='/confirmation' element={<ConfirmRegister />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    );
-  } else {
     return (
       <div className="App">
         <Header />
+        <Routes>
+          <Route path='/confirmation' element={<ConfirmRegister />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
         <Footer />
       </div>
     );
-  }
 }
 
 export default App;
--- ybook/src/components/ListPosts.tsx
+++ ybook/src/components/ListPosts.tsx
@@ -1,36 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { User } from '../hooks/useAuth';
+import usePost from '../hooks/usePost';
+import { usePostData } from '../providers/PostProvider';
 import SinglePost from './SinglePost';
 
 export interface ListPostsPage {}
 
-export type Post = {
-  createdAt: string;
-  htmlContent: string;
-  id: number;
-  userId : number;
-  user : User;
-  postLikes : Like
-}
-
-export type Like = {
-  userId : number
-}
-
 const ListPosts : React.FC<ListPostsPage> = () => {
-  const [posts, setPosts] = useState<Post[]|undefined>();
-
-  useEffect(() => {
-    fetch('http://localhost:3100/post/')
-      .then(response => response.json())
-      .then(res => setPosts(res))
-      .catch(err => console.log(err))
-  }, []);
+  const {posts} = usePostData();
 
   return (
     <div>
       <h1>ListPosts</h1>
-      <SinglePost posts={posts ?? []} setPosts={setPosts} />
+      {posts?.map((post) => {
+        return (
+          <SinglePost key={post.id} post={post ?? []} />
+        )
+      })}
     </div>
   )
 }
