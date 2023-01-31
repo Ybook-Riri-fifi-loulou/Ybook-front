0eeec36db2cd56b3be5eae67914c3a9ed9eba881
Arnal Théo <61836279+TheoArnal@users.noreply.github.com>
2023-01-31 22:02:46 +0100

    page profil update

--- ybook/src/Layouts/Profil.tsx
+++ ybook/src/Layouts/Profil.tsx
@@ -5,57 +5,20 @@ import useModal from "../hooks/useModal";
 import ListPosts from "../components/ListPosts";
 import {PostProvider} from "../providers/PostProvider";
 import ProfilTabs from "../components/ProfilTabs";
+import {useGlobal} from "../providers/GlobalProvider";
 
 export interface ProfilProps {
 }
 
 const Profil: React.FC<ProfilProps> = () => {
-    const [profil, setProfil] = useState<Object | undefined>();
-    const {dataURLToFile} = useProfil();
-    const {isOpen, toggle} = useModal();
-
+    const {capture, setPicture, picture} = useProfil();
 
     const videoConstraints = {
         width: 150,
         height: 150,
         facingMode: 'user',
     }
-    const [picture, setPicture] = useState<any>('')
     const webcamRef = React.useRef<Webcam>(null)
-    let url = '';
-    let key = 'image/rifilou';
-
-    const capture = React.useCallback(
-        async () => {
-            const imageSrc = webcamRef.current?.getScreenshot()
-            setPicture(imageSrc)
-            let file = dataURLToFile(imageSrc, 'test.jpg')
-            if (file !== undefined) {
-                await fetch('http://localhost:3100/post/presignedurl')
-                    .then(response => response.json())
-                    .then(res => {
-                        url = res.url
-                        key = res.key
-                    })
-                    .catch(err => console.log(err))
-                await fetch(url, {
-                    method: 'PUT',
-                    body: file,
-                }).then(response => {
-                    console.log(response)
-                    console.log(key)
-                }).catch(err => console.log(err))
-                //body vide je sais pas pourquoi
-                await fetch('http://localhost:3100/user/28/profilePicture', {
-                    method: 'PUT',
-                    body: key,
-                }).then(response => {
-                    console.log(response)
-                }).catch(err => console.log(err))
-            }
-        },
-        [webcamRef]
-    )
 
     return (
         <div className="mt-5 text-center">
@@ -82,7 +45,7 @@ const Profil: React.FC<ProfilProps> = () => {
                         <button
                             onClick={(e) => {
                                 e.preventDefault()
-                                setPicture(webcamRef.current?.getScreenshot())
+                                    setPicture(webcamRef.current?.getScreenshot())
                             }}
                             className="btn btn-primary"
                         >
@@ -92,7 +55,7 @@ const Profil: React.FC<ProfilProps> = () => {
                         <button
                             onClick={(e) => {
                                 e.preventDefault()
-                                capture()
+                                capture(webcamRef)
                             }}
                             className="btn btn-danger"
                         >
