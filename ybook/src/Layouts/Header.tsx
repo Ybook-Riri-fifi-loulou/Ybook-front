ad95ffe59ee0376b77a7957202164c45b2bfe0e9
Lucas Dindault <lucas@creabilis.com>
2023-02-02 00:05:26 +0100

    integration profil header & fix css header / footer

--- ybook/src/Layouts/Header.tsx
+++ ybook/src/Layouts/Header.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
-import { AiOutlineMenu, AiOutlineSetting } from 'react-icons/ai'
+import { AiOutlinePoweroff } from 'react-icons/ai'
+import { HiOutlineUserCircle } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
 
 function Header() {
+    const { logout } = useAuth();
+
     return (
         <>
             <div className="header">
                 <div className="header-content">
-                    <div className="header-item" id="header-logo">
-                        <img src={logo} alt="logo" className="img-fluid" />
+                    <div className="header-item">
+                        <Link to='/profil'>
+                            <HiOutlineUserCircle className='header-item__icon' />
+                        </Link>
                     </div>
-                    <div className="header-item" id="menu-burger">
-                        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><AiOutlineSetting /></button>
+                    <div className="header-item">
+                        <Link to='/'>
+                            <img src={logo} alt="logo" className="img-fluid" width={40} loading='lazy'/>
+                        </Link>
+                    </div>
+                    <div className="header-item">
+                        <Link to='/login' onClick={logout}>
+                            <AiOutlinePoweroff className='header-item__icon' />
+                        </Link>
                     </div>
                 </div>
             </div>
